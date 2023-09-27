050d1dab77717bab0f1c1f6ea8dd65c31b6c5ac3
ルスラン ブドビキン <banievro@gmail.com>
2023-09-28 02:22:19 +0900

    Version 0.2

--- src/FileEncodingSorter.java
+++ src/FileEncodingSorter.java
@@ -7,30 +7,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
-import java.io.FileInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.FilenameFilter;
 
 public class FileEncodingSorter {
     private static final Logger logger = LoggerFactory.getLogger(FileEncodingSorter.class);
     private static final String folderPath = "C:\\Unicode and ANSI encoding sorter\\Sorter folder";
     private static final Map<String, String> encodingToFolderMap = new HashMap<>();
+    private static final String txtFileExtension = ".txt";
 
     static {
         // Заполните маппинг кодировок и соответствующих папок здесь
-        encodingToFolderMap.put("UTF-8", "UTF8_Files");
-        encodingToFolderMap.put("ISO-8859-1", "ISO8859_1_Files");
-        encodingToFolderMap.put("ANSI", "ANSI_Files");
-        encodingToFolderMap.put("UTF-16 LE", "UTF-16 LE_Files");
-        encodingToFolderMap.put("UTF-16 BE", "UTF-16 BE_Files");
+        encodingToFolderMap.put("ISO-8859-1", "ANSI");
+        encodingToFolderMap.put("ANSI", "ANSI");
+        encodingToFolderMap.put("Windows-1251", "ANSI");
+        encodingToFolderMap.put("Shift_JIS", "ANSI");
+        encodingToFolderMap.put("windows-1252", "UTF8");
+        encodingToFolderMap.put("UTF-16 LE", "UTF8");
+        encodingToFolderMap.put("UTF-16 BE", "UTF8");
+        encodingToFolderMap.put("UTF-8", "UTF8");
         // Добавьте другие кодировки и соответствующие папки по мере необходимости
     }
 
     public static void main(String[] args) {
-        createSortingFolders();
-        sortFilesByEncoding();
+        // Сначала получим список .txt файлов в папке
+        File folder = new File(folderPath);
+        File[] txtFiles = folder.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(txtFileExtension);
+            }
+        });
+
+        if (txtFiles != null) {
+            int numberOfTxtFiles = txtFiles.length;
+            logger.info("Number of .txt files in the folder: {}", numberOfTxtFiles);
+
+            // Затем можно приступить к созданию папок и сортировке файлов
+            createSortingFolders();
+            copyFilesByEncoding();
+        } else {
+            logger.info("No .txt files found in the folder.");
+        }
     }
 
     private static void createSortingFolders() {
@@ -46,15 +70,16 @@ public class FileEncodingSorter {
         }
     }
 
-    private static void sortFilesByEncoding() {
+    private static void copyFilesByEncoding() {
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
 
         if (files != null) {
             for (File file : files) {
-                if (file.isFile()) {
+                if (file.isFile() && file.getName().toLowerCase().endsWith(txtFileExtension)) {
                     try {
-                        // Используйте Apache Tika для определения кодировки файла
+                        logger.info("Processing file: {}", file.getAbsolutePath());
+
                         FileInputStream inputStream = new FileInputStream(file);
                         TikaInputStream tikaInputStream = TikaInputStream.get(inputStream);
                         Metadata metadata = new Metadata();
@@ -63,23 +88,36 @@ public class FileEncodingSorter {
                         EncodingDetector detector = new DefaultEncodingDetector();
                         Charset charset = detector.detect(tikaInputStream, metadata);
 
-                        // Определите папку на основе кодировки
-                        String encoding = charset.name();
+                        String encoding;
+                        if (charset == null) {
+                            // Handle files with unknown encoding
+                            logger.error("Unable to detect encoding for file: {}", file.getAbsolutePath());
+                            continue;
+                        } else {
+                            encoding = charset.name();
+                            // Add this line to log the detected encoding
+                            logger.info("Detected encoding for file '{}': {}", file.getAbsolutePath(), encoding);
+                        }
+
+                        // Если файл обнаружен как ANSI, но не содержит не-ASCII символов,
+                        // предположим, что это на самом деле UTF-8.
+                        if ("ANSI".equals(encoding) && isAscii(file)) {
+                            encoding = "UTF-8";
+                        }
+
                         String targetFolderName = encodingToFolderMap.get(encoding);
 
-                        // Переместите файл в соответствующую папку
                         if (targetFolderName != null) {
                             File targetFolder = new File(folder, targetFolderName);
                             if (!targetFolder.exists()) {
                                 if (!targetFolder.mkdirs()) {
                                     logger.error("Failed to create target folder: {}", targetFolder.getAbsolutePath());
-                                    continue; // Пропустить этот файл и перейти к следующему
+                                    continue;
                                 }
                             }
                             File targetFile = new File(targetFolder, file.getName());
-                            if (!file.renameTo(targetFile)) {
-                                logger.error("Failed to move file from '{}' to '{}'", file.getAbsolutePath(), targetFile.getAbsolutePath());
-                            }
+                            Files.copy(file.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                            logger.info("File '{}' copied to '{}'.", file.getAbsolutePath(), targetFile.getAbsolutePath());
                         }
                     } catch (IOException e) {
                         logger.error("An error occurred while processing a file:", e);
@@ -87,5 +125,15 @@ public class FileEncodingSorter {
                 }
             }
         }
+    }
+
+    private static boolean isAscii(File file) throws IOException {
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        for (byte b : bytes) {
+            if (b < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
